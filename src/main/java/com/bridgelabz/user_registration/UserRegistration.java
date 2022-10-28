3db0bf13a8a4b3a80859f52d00597d263dabc272
smutallib97 <smutallib97@gmail.com>
2022-10-28 06:03:08 +0530

    Added: User first name test case

--- src/main/java/com/bridgelabz/user_registration/UserRegistration.java
+++ src/main/java/com/bridgelabz/user_registration/UserRegistration.java
@@ -1,5 +1,14 @@
 package com.bridgelabz.user_registration;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UserRegistration {
-    
+    //UC1 - Enter Valid USer First Name
+    public boolean firstName(String firstName) {
+        String regex = "^[A-Z]{1}[a-z]{2,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        return matcher.matches();
+    }
 }
--- src/test/java/com/bridgelabz/user_registration/UserRegistrationTest.java
+++ src/test/java/com/bridgelabz/user_registration/UserRegistrationTest.java
@@ -1,7 +1,23 @@
 package com.bridgelabz.user_registration;
 
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserRegistrationTest {
+    UserRegistration userRegistration = new UserRegistration();
 
+    //UC1 - Enter Valid USer First Name
+    @Test
+    public void givenFirstName_WhenValid_ShouldReturnTrue() {
+        boolean result = userRegistration.firstName("Sayyed");
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void givenFirstName_WhenInvalid_ShouldReturnFalse() {
+        boolean result = userRegistration.firstName("sayyed");
+        Assertions.assertEquals(false, result);
+    }
 }
